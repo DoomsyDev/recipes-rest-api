b687d9ea574db1b82c9bfe73dea8d2d0af0507bd
Rúben Barbosa <ru.borgs@gmail.com>
2025-11-17 10:00:43 +0000

    created project and all the files in it

---
+++ RestApi/Data/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+using Microsoft.EntityFrameworkCore;
+using RestApi.Models;
+
+namespace RestApi.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options) { }
+
+        public DbSet<Recipe> Recipes { get; set; }
+    }
+}
---
+++ RestApi/Models/Recipe.cs
@@ -0,0 +1,9 @@
+namespace RestApi.Models
+{
+    public class Recipe
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+    }
+}
